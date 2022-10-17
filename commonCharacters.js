9fc2263c1bc7db5dc9da9cccd4013b3ae37e24af
hamzagouissem <gouissemhamza7@gmail.com>
2022-10-17 11:16:38 +0100

    save

--- commonCharacters.js
+++ commonCharacters.js
@@ -12,10 +12,38 @@
 
 
 
- var commonCharacters = function(string1, string2) {
+ var commonCharacters = function(a,b) {
+
+  var commonCharacters = function(string1, string2) {
+    
+  
+    let temp = [];
+    let result = "";
+  
+    if (string1 === string2) {
+      result = string1;
+    } else if (string1.length === 0 || string2.length === 0) {
+      result = "";
+    } else {
+      // two for loops
+      for (let i = 0; i < string1.length; i++) {
+        for (let j = 0; j < string2.length; j++) {
+         
+          if (string1[i] === string2[j] && !temp.includes(string2[j])) {
+            temp.push(string2[j]);
+          }
+        }
+      }
+      // Convert temp array into String
+      result = temp.join("");
+    }
+    return result;
+  };
+
     // TODO: Your code here!
     
   }
   
-  
+  // var commonCharacters = function(a,b) {
+    
  
